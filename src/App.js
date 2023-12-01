c60c0d1e184e15a473e99e9d30965e90c08e078d
IgorOdaryuk <odaryukigor@gmail.com>
2023-12-01 14:24:14 +0200

    Added some files to the code structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import {useState} from 'react'
+import { IconButton } from '@mui/material';
+import { CheckCircle, Edit, Delete } from '@mui/icons-material';
 import './App.css';
 
 function App() {
-  const [task, setTask] = useState([])
+
+  const dummyData = [
+    {id: 1, task: "Task 1", status: false},
+    {id: 2, task: "Task 2", status: false},
+    {id: 3, task: "Task 3", status: false},
+  ]
+
+  const [task, setTask] = useState(dummyData)
   const [addInput, setAddInput] = useState('')
   const [editInput, setEditInput] = useState('')
   const [editId, setEditId] = useState('')
@@ -24,7 +33,23 @@ function App() {
       </div>
     </div>
         
-        <div className="content"></div>
+        <div className="content">
+          {task.map((item) => {
+            return (
+              <div key={item.id} className="listItem">
+                <p>{item.task}</p>
+
+                <div className="actionBtns">
+                <IconButton>
+                  <CheckCircle />
+                </IconButton>
+                  <button>Edit</button>
+                  <button>Delete</button>
+                </div>
+              </div>
+            )
+          })}
+        </div>
     </div>
 }
 
